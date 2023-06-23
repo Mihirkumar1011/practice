59d3a97d79854d2db8e582dbb38e2a492ec62d71
Mihirkumar Patel <pate3902@sheridancollege.ca>
2023-06-23 00:08:28 -0400

    practice_first_commit

---
+++ src/hello_world/Hello_World.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_world;
+
+/**
+ *
+ * @author mihir
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
